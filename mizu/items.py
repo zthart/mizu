68afff7229663bc3bf7af599ecb3ac27d8f94d07
Zachary <zach@csh.rit.edu>
2019-02-19 11:16:10 -0500

    skeletons for item endpoints, ignoring ldap/auth for the moment

--- mizu/items.py
+++ mizu/items.py
@@ -9,7 +9,24 @@ CRUD for Items
 
 from flask import Blueprint, jsonify, request
 
+from mizu.models import Machine
+from mizu.models import Slot
+from mizu.models import Item
+
 from mizu import db
 
 items_bp = Blueprint('items_bp', __name__)
+
+
+@items_bp.route('/items', methods=['GET', 'POST', 'DELETE'])
+def manage_items():
+    pass
+
+@items_bd.route('/items/price', methods=['POST'])
+def update_item_price():
+    pass
+
+@items_db.route('/items/name', methods=['POST'])
+def update_item_name():
+    pass
 
