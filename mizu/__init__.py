3c42fc31454ec58e5b6872df2429b4ebff311afe
Zachary <zach@csh.rit.edu>
2019-04-30 18:46:45 -0400

    methods

--- mizu/__init__.py
+++ mizu/__init__.py
@@ -64,5 +64,6 @@ def handle_404(e):
 def allow_cors(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
     return response
 
