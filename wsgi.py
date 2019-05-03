1572d1e416243ff9880c72eca1c1de14e62608ae
Zachary <zach@csh.rit.edu>
2019-05-03 18:13:26 -0400

    threaded=False in wsgi a la https://github.com/pallets/flask/issues/2744

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ import os
 from mizu import app
 
 if __name__ == "__main__":
-    app.run(host=app.config['IP'], port=app.config['PORT'])
+    app.run(host=app.config['IP'], port=app.config['PORT'], threaded=False)
 
 application = app
 
