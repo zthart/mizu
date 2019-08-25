8f4ae6d96460ed09568fd4739b3f7317ef40bedc
Zachary Hart <zthart96@gmail.com>
2019-08-25 12:38:15 -0400

    Distinguish between active/disabled and stocked/empty

--- mizu/drinks.py
+++ mizu/drinks.py
@@ -47,20 +47,39 @@ def current_drinks(adapter):
 
     response = {
         "machines": []
-    }
+    } 
+
     for machine in machines:
         machine_slots = adapter.get_slots_in_machine(machine['name'])
-        machine_contents = {'id': machine['id'], 'name': machine['name'], 'display_name': machine['display_name'], 'slots': []}
+        
+        is_online = True
+        
+        try:
+            slot_status = _get_machine_status(machine['name'])
+        except requests.exceptions.ConnectionError:
+            slot_status = [{'empty': True} for n in range(len(machine_slots))]
+            is_online = False  # seems a useful feature
+        
+        machine_contents = {
+            'id': machine['id'], 
+            'name': machine['name'], 
+            'display_name': machine['display_name'],
+            'is_online': is_online,
+            'slots': []
+        }
+
         for slot in machine_slots:
             slot_item = adapter.get_item(slot['item'])
             machine_contents['slots'].append({
                 "number": slot['number'],
                 "active": slot['active'],
+                "empty": slot_status[slot['number']-1]['empty'],
                 "item": {
                     "name": slot_item['name'],
                     "price": slot_item['price'],
                     "id": slot_item['id'],
                 },
+
             })
         response['machines'].append(machine_contents)
 
@@ -103,6 +122,13 @@ def drop_drink(user = None):
             body['slot']
         ))
 
+    slot_status = _get_machine_status(body['machine'])
+    if slot_status[body['slot']-1]['empty']:  # slots are 1 indexed
+        return jsonify({
+            "error": "The requested slot is empty!",
+            "errorCode": 400
+        }), 400
+
     item = db.session.query(Item).filter(Item.id == slot.item).first()
 
     if bal_before < item.price:
@@ -114,7 +140,7 @@ def drop_drink(user = None):
 
     machine_hostname = '{}.csh.rit.edu'.format(machine.name)
     request_endpoint = 'https://{}/drop'.format(machine_hostname)
-
+    
     body = {
         "slot": slot.number
     }
@@ -136,11 +162,51 @@ def drop_drink(user = None):
     try:
         response.raise_for_status()
     except requests.exceptions.HTTPError:
-        return jsonify({"error": "Could not access slot for drop!", "errorCode": response.status_code}),\
+        return jsonify({"error": "Could not access slot for drop!",
+                        "message": response.json()['error'],
+                        "errorCode": response.status_code}),\
                        response.status_code
 
     new_balance = bal_before - item.price
     _manage_credits(user['preferred_username'], new_balance)
 
     return jsonify({"message": "Drop successful!", "drinkBalance": new_balance}), response.status_code
+
+def _get_machine_status(machine_name):
+    """ helper function to query a machine given it's name (should be the actual hostname, will be dropped into a
+        {}.csh.rit.edu template), and return slot status information in a more programmatically useful way.
+
+        Realistically, the data should look like this coming back from the mahcine -- this is low hanging fruit for someone
+        to fix.
+
+        Warning::
+
+            This doesn't actually validate the machine name. That should be done in what ever route needs to call this function.
+
+        Raises:
+            requests.exceptions.HTTPError: in the event the machine responds with a non-2XX
+    """
+
+    headers = {
+        'X-Auth-Token': app.config['MACHINE_API_TOKEN'],
+        'Content-Type': 'application/json'
+    }
+
+    endpoint = 'https://{}.csh.rit.edu/health'.format(machine_name)
+
+    health_status = requests.get(endpoint, headers=headers)
+    health_status.raise_for_status()
+
+    health_results = health_status.json()
+
+    slots = []
+
+    for idx, slot in enumerate(health_results['slots'], 1):
+        slots.append({
+            'number': idx,
+            'w1id': slot.split('(')[1].split(')')[0],
+            'empty': 'empty' in slot
+        })
+
+    return slots
 
